6e707f8b3e7096c43a2149b8560086adf5edfab6
LordElf <shawnlai.cn@gmail.com>
2020-09-16 23:58:23 -0400

    chiAdd

--- src/calculator.py
+++ src/calculator.py
@@ -1,16 +1,36 @@
-from math import *
 import numpy as np
+from math import *
 
-data = input()
+data = "2.0	    1.1	    6.0	    1.6	5.4	0.4	    1.0	    5.3 15.8	0.7	4.8	0.9	    12.1	    5.3	0.6	" 
 data = data.replace(',', ' ')
 data = data.split()
 print(data)
 
-#.23 32, 32.3 .2
+# .23 32, 32.3 .2
 data = np.array(data).astype(np.float)
 data = np.sort(data)
 sum = np.sum(data)
 print(data)
 print('sum: ', sum)
-std = np.std(data)
-print('std: ', std)
+print(len(data))
+mean = np.mean(data)
+stDev = np.std(data)
+print('mean', mean)
+print('stDev: ', stDev)
+
+# normal distribution
+print('normal')
+z = 1.96
+range = z * stDev / sqrt(len(data))
+ciL = mean + range
+ciS = mean - range
+print('range: ', range)
+print('CI: (', ciS, ciL, ')')
+
+# chi disctribution
+print('chi')
+x1 = 46.979
+x2 = 16.791
+ciL = 2 * sum / x2
+ciS = 2 * sum / x1
+print('CI: (', ciS, ciL, ')')
